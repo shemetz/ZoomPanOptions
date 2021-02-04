10cd64cf0453fe5267afc9b46ca8c0ce538a823a
itamar <itamarcu@gmail.com>
2021-02-04 22:30:48 +0200

    quick fix to the bug I introduced today

--- scripts/zoom-pan-options.js
+++ scripts/zoom-pan-options.js
@@ -31,12 +31,10 @@ function _onWheel_Override (event) {
   // Case 1 - rotate stuff
   if (layer instanceof PlaceablesLayer) {
     const hasTarget = layer.options?.controllableObjects ? layer.controlled.length : !!layer._hover
-    if (hasTarget) {
-      const t = Date.now()
-      if ((t - game.keyboard._wheelTime) < game.keyboard.constructor.MOUSE_WHEEL_RATE_LIMIT) return
-      game.keyboard._wheelTime = t
-      return layer._onMouseWheel(event)
-    }
+    if (!hasTarget) return
+    const t = Date.now()
+    if ((t - game.keyboard._wheelTime) < game.keyboard.constructor.MOUSE_WHEEL_RATE_LIMIT) return
+    game.keyboard._wheelTime = t
     if (mode === 'Default' && (ctrlOrMeta || shift)) {
       return layer._onMouseWheel(event)
     }
